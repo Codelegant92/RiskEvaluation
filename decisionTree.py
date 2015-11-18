6d287805d3491f3f7de7306e996b1bed91e8e18a
RobinCHEN <robinchan1992@gmail.com>
2015-11-18 22:52:51 +0800

    1118 commit: 人为控制bagging过程中正负训练样本数量相同,最终结果type1 accuracy 和 type2 accuracy基本相差不大.

--- decisionTree.py
+++ decisionTree.py
@@ -5,7 +5,7 @@ from sklearn import tree
 from sklearn.ensemble import AdaBoostClassifier, RandomForestClassifier
 from sklearn.externals.six import StringIO
 import os
-import pydot
+#import pydot
 
 def decision_Tree(trainFeature, trainLabel, testFeature):
     clf = tree.DecisionTreeClassifier()
@@ -40,33 +40,53 @@ def adboostDT(trainFeature, trainLabel, testFeature, estimatorNum = 50, learning
 def bagging_adboostDT(trainFeature, trainLabel, testFeature, estimatorNum = 50, learningRate = 1.0):
     folderNum = 5
     predictedLabel_voting = []
-    '''
-    posNum = list(trainLabel.count(1))
-    negNum = list(trainLabel.count(0))
-    posFeature = list(trainFeature)
+
+    posNum = list(trainLabel).count(1)
+    negNum = list(trainLabel).count(0)
+    trainFeature = list(trainFeature)
+    posFeature = []
     negFeature = []
-    for i in range(trainLabel.shape[1]):
-        if(trainLabel[i] == 0):
-            negFeature.append(posFeature.pop(i))
+    for i in range(trainLabel.shape[0]):
+        try:
+            if(trainLabel[i] == 0):
+                negFeature.append(trainFeature[i])
+            else:
+                posFeature.append(trainFeature[i])
+        except ValueError, e:
+            print("error", e, "on line", i)
+
     if(posNum < negNum):
         negFeatureFolders = []
         sequence = range(negNum)
         for i in range(folderNum):
             random.shuffle(sequence)
             negFeatureFolders.append([negFeature[j] for j in sequence[:posNum]])
+    print(np.array(negFeatureFolders).shape)
     for i in range(folderNum):
-        subTrainFeature = posFeature
-        subTrainFeature.extend(negFeatureFolders[i])
-        subTrainLabel =
+        subTrainFeature = negFeatureFolders[i]
+        subTrainFeature.extend(posFeature)
+        subTrainFeature = np.array(subTrainFeature)
+        subTrainLabel = list(np.zeros(posNum))
+        subTrainLabel.extend(list(np.ones(posNum)))
+        subTrainLabel = np.array(subTrainLabel)
+        print("=====%dst Bagging=====") % (i+1)
+        print("Positive: %d, Negative: %d") % (list(subTrainLabel).count(1), list(subTrainLabel).count(0))
+        print(subTrainFeature.shape)
+        print(subTrainLabel)
+        clf = AdaBoostClassifier(n_estimators = estimatorNum, learning_rate = learningRate)
+        clf.fit(subTrainFeature, subTrainLabel)
+        predictedLabel_temp = clf.predict(testFeature)
+        predictedLabel_voting.append(predictedLabel_temp)
+        print("%dst predicted labels:") % (i+1)
+        print(predictedLabel_temp)
+    predictedLabel_voting = np.array(predictedLabel_voting).T
+    predictedLabel = [1 if(list(predictedLabel_voting[i]).count(1) > list(predictedLabel_voting[i]).count(0)) else 0 for i in range(predictedLabel_voting.shape[0])]
+    print(predictedLabel)
+    return(predictedLabel)
+
+
+
     '''
-
-
-
-
-
-
-
-
     randomFeatureFolder, randomLabelFolder = crossValidation(trainFeature, trainLabel, folderNum)
     print("========bagging adaDT========")
     for i in range(folderNum):
@@ -90,7 +110,7 @@ def bagging_adboostDT(trainFeature, trainLabel, testFeature, estimatorNum = 50, 
     predictedLabel = [1 if(list(predictedLabel_voting[i]).count(1) > list(predictedLabel_voting[i]).count(0)) else 0 for i in range(predictedLabel_voting.shape[0])]
     print(predictedLabel)
     return(predictedLabel)
-
+    '''
 if __name__ == "__main__":
     folderNum = 5
     #dataFeature, dataLabel = readAustralianData('./Data/Australia/australian.dat')
